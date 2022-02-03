d4ce05f2a4dcd51c7cad8307e3653700621cec0a
Yourusername <jkidder1@vols.utk.edu>
2022-02-03 09:14:55 -0500

    Proto-App

--- server.R
+++ server.R
@@ -1,10 +1,14 @@
-library(shiny)
-library(fpp3)
-
-sales_df <- read.csv("data/clean_sales.csv")
-sales_df$date <- as.Date(sales_df$date)
-sales_df <- as_tsibble(sales_df, key = c("scode", "pcode"), index = "date")
 
 server <- function(input, output) {
+  output$debug <- renderPrint({input$selected_store})
+  output$debug <- renderPrint({input$selected_sku})
+  output$debug <- renderPrint({
+  sales_df[sales_df$scode==input$selected_store & sales_df$pcode==input$selected_sku,]
+  })
+
+  output$ts_plot <- renderPlot({
+    plot_df <- sales_df[sales_df$scode==input$selected_store & sales_df$pcode==input$selected_sku,]
+  autoplot(plot_df)
+    })
 
 }
